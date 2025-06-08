41795ba477febc09de749b49908e520197a949c4
Hossein Abbasizadeh <77976684+abbasizadeh@users.noreply.github.com>
2025-06-08 13:26:25 +0200

    Add files via upload

---
+++ Rscript/sig_high_q_dur/1_BN_development_high_q_dur.R
@@ -0,0 +1,102 @@
+################################################################################
+#                                                                              #
+#                       learn causal graph for high q dur                      #
+#                                PC Algorithm                                  #
+# ##############################################################################
+setwd("~/Catchment-Causal-Discovery/")
+
+# get the data
+source('./source/1_load_data_&_packages.R')
+# load functions
+source('./source/2_functions.r')
+
+target_node <- 'high_q_dur'
+
+# add the target variable to the selected variables
+selected_data_slope_fdc <- 
+  merge(selected_data_slope_fdc, ALL_SIGNATURES[, c('gauge_id', 'high_q_dur')], by = 'gauge_id')
+
+selected_data_slope_fdc <- na.omit(selected_data_slope_fdc)
+
+# transform data to normal
+data_slope_fdc <- transform_boxcox(selected_data_slope_fdc[,-c('gauge_id')])
+
+# scale the data
+data_slope_fdc[, names(data_slope_fdc) := lapply(.SD, scale)]
+
+# black list 
+blacklist_pc[from == 'target_node', from := (target_node)]
+
+# run algorithms ---------------------------------------------------------------
+set.seed(123)
+pc.fit <- pc.stable(data_slope_fdc, test = 'mi-g-sh', blacklist = blacklist_pc, alph = 0.2)
+parents(x = pc.fit, node = 'high_q_dur')
+graphviz.plot(pc.fit)
+saveRDS(object = pc.fit, file = './data/output/PC_DAGs/pc_mi_g_sh_hqd.rds')
+graphviz.plot(readRDS(file = './data/output/PC_DAGs/pc_mi_g_sh_hqd.rds'))
+
+
+# use bootstrap to check for stability
+set.seed(123)
+pc_boot_strength <- 
+  boot.strength(
+    data_slope_fdc, 
+    R = 1000, 
+    algorithm = 'pc.stable',  
+    algorithm.args = list(test = 'mi-g-sh', blacklist = blacklist_pc, alpha = 0.05)
+  )
+
+# str(pc_boot_strength)
+saveRDS(object = pc_boot_strength, file = './data/output/PC_DAGs/boot_pc_mi_g_sh_hqd.rds')
+
+dt_boot <- as.data.table(pc_boot_strength)
+dt_boot[strength > 0 & to == (target_node),]
+
+# get the parents of the signature
+vec_parent_nodes_boot <- dt_boot[strength > 0 & to == (target_node), from]
+
+
+# Generate the averaged network using a threshold of 0.1 for edge strength
+avg_net <- averaged.network(pc_boot_strength, threshold = 0.1)
+graphviz.plot(avg_net)
+parents(avg_net, node = target_node)
+avg_net
+
+
+# read the likelihood ratio test
+dt_hqd_reg_test <- 
+  readRDS(file = './data/output/regression_test/likelihood_ratio_test_high_q_dur')
+
+dt_target_node_test <- dt_hqd_reg_test[target_1 == (target_node) | target_2 == (target_node),]
+dt_target_node_test <- dt_target_node_test[target_1 %in% vec_parent_nodes_boot | target_2 %in% vec_parent_nodes_boot,]
+# dt_target_node_test[p_value_fwd < 0.05 | p_value_bwd < 0.06]
+
+dt_target_node_test
+dt_boot[strength > 0 & to == (target_node),]
+
+
+
+# CPDAG to DAG-----------------------------------------------------------------
+pc.fit <- readRDS(file = './data/output/PC_DAGs/pc_mi_g_sh_hqd.rds')
+parents(x = pc.fit, node = 'high_q_dur')
+plot(pc.fit)
+
+# define edges
+pc.fit <- set.arc(pc.fit, from = 'p_mean', to = 'high_prec_freq')
+pc.fit <- set.arc(pc.fit, from = 'soil_porosity', to = 'sand_frac')
+
+pc.fit <- set.arc(pc.fit, from = 'sand_frac', to = 'soil_conductivity') # to be able to change undirected to directed
+pc.fit <- set.arc(pc.fit, from = 'clay_frac', to = 'soil_conductivity')
+
+pc.fit <- set.arc(pc.fit, from = 'elev_mean', to = 'slope_mean')
+
+pc.fit
+
+pc.fit
+saveRDS(object = pc.fit, file = './data/output/PC_DAGs/DAG_hqd.rds')
+
+plot(pc.fit)
+
+
+
+
---
+++ Rscript/sig_high_q_dur/2_prediction_high_q_dur.r
@@ -0,0 +1,122 @@
+# load variables and packages
+
+# working directory
+setwd("~/Catchment-Causal-Discovery/")
+
+# load packages and data
+source('./source/1_load_data_&_packages.R')
+
+# load functions
+source('./source/2_functions.r')
+
+sampling_numbers <- 500
+# source('./Rscripts/source/6_1_prediction_all_data.R')
+
+
+# baseflow index ---------------------------------------------------------------
+target_node <- 'high_q_dur'
+pc.fit <- readRDS('./data/output/PC_DAGs/DAG_hqd.rds')
+
+# add the target variable to the selected variables
+data_for_prediction <- 
+  merge(selected_data_slope_fdc, ALL_SIGNATURES[, c('gauge_id', 'high_q_dur')], by = 'gauge_id')
+data_for_prediction <- na.omit(data_for_prediction)
+
+# source('./source/6_1_prediction_all_data.R')
+# 
+# saveRDS(object = prediction_results_whole_data, 
+#         file = './data/output/prediciton_results/preciction_results_whole_data_high_q_dur.rds')
+# 
+# rm(prediction_results_whole_data, data_for_prediction)
+# 
+# 
+# # prediction in clusters =======================================================
+# # geol clustering --------------------------------------------------------------
+# source('./source/5_1_geol_clustering.R')
+# 
+# data_for_prediction <- 
+#   merge(selected_data_slope_fdc, ALL_SIGNATURES[, c('gauge_id', 'high_q_dur')], by = 'gauge_id')
+# data_for_prediction <- na.omit(data_for_prediction)
+# 
+# 
+# source('./source/6_2_prediction_cluster.R')
+# 
+# saveRDS(object = prediction_results_cluster, 
+#         file = './data/output/prediciton_results/preciction_results_geol_clustering_high_q_dur.rds')
+# 
+# selected_data_slope_fdc[, cluster_hc := NULL]
+# rm(prediction_results_cluster, data_for_prediction)
+# 
+# # soil clustering ----------------------------------------------------------
+# source('./source/5_2_soil_clustering.R')
+# 
+# data_for_prediction <- 
+#   merge(selected_data_slope_fdc, ALL_SIGNATURES[, c('gauge_id', 'high_q_dur')], by = 'gauge_id')
+# data_for_prediction <- na.omit(data_for_prediction)
+# 
+# 
+# source('./source/6_2_prediction_cluster.R')
+# 
+# saveRDS(object = prediction_results_cluster, 
+#         file = './data/output/prediciton_results/preciction_results_soil_clustering_high_q_dur.rds')
+# 
+# selected_data_slope_fdc[, cluster_hc := NULL]
+# rm(prediction_results_cluster, data_for_prediction)
+# 
+# 
+# # topo clustering ----------------------------------------------------------
+# source('./source/5_3_topo_clustering.R')
+# 
+# data_for_prediction <- 
+#   merge(selected_data_slope_fdc, ALL_SIGNATURES[, c('gauge_id', 'high_q_dur')], by = 'gauge_id')
+# data_for_prediction <- na.omit(data_for_prediction)
+# 
+# 
+# source('./source/6_2_prediction_cluster.R')
+# 
+# saveRDS(object = prediction_results_cluster, 
+#         file = './data/output/prediciton_results/preciction_results_topo_clustering_high_q_dur.rds')
+# 
+# selected_data_slope_fdc[, cluster_hc := NULL]
+# rm(prediction_results_cluster, data_for_prediction)
+# 
+# 
+# # vege clustering ----------------------------------------------------------
+# source('./source/5_4_vege_clustering.R')
+# 
+# data_for_prediction <- 
+#   merge(selected_data_slope_fdc, ALL_SIGNATURES[, c('gauge_id', 'high_q_dur')], by = 'gauge_id')
+# data_for_prediction <- na.omit(data_for_prediction)
+# 
+# 
+# source('./source/6_2_prediction_cluster.R')
+# 
+# saveRDS(object = prediction_results_cluster, 
+#         file = './data/output/prediciton_results/preciction_results_vege_clustering_high_q_dur.rds')
+# 
+# selected_data_slope_fdc[, cluster_hc := NULL]
+# rm(prediction_results_cluster, data_for_prediction)
+
+
+# climate clustering ----------------------------------------------------------
+source('./source/5_5_climate_clustering.R')
+
+data_for_prediction <- 
+  merge(selected_data_slope_fdc, ALL_SIGNATURES[, c('gauge_id', 'high_q_dur')], by = 'gauge_id')
+data_for_prediction <- na.omit(data_for_prediction)
+
+
+source('./source/6_2_prediction_cluster.R')
+
+saveRDS(object = prediction_results_cluster, 
+        file = './data/output/prediciton_results/preciction_results_climate_clustering_high_q_dur.rds')
+
+selected_data_slope_fdc[, cluster_hc := NULL]
+# rm(prediction_results_cluster, data_for_prediction)
+
+
+
+
+
+
+
